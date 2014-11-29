3b8ab51e4d28e12601650d9166a773c1077ef6be
tron <tron@tronsoft.nl>
2014-11-29 22:45:06 +0100

    Added example application

---
+++ Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+
+using WiringPiSharp;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Setup.WiringPiPiSetupGpio();
+            GPIO.PinMode(18, WiringPi.Output);
+            while (true)
+            {
+                GPIO.DigitalWrite(18, WiringPi.High);
+                Thread.Sleep(500);
+                GPIO.DigitalWrite(18, WiringPi.Low);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
